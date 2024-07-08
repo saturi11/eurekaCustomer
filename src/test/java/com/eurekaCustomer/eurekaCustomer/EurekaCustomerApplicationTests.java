d11d15ad9bd105e0fe0dcf63415f5e5cc083358e
Gabriel Saturi <61097598+saturi11@users.noreply.github.com>
2024-07-08 12:35:01 -0300

    Add files via upload

---
+++ src/test/java/com/eurekaCustomer/eurekaCustomer/EurekaCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaCustomer.eurekaCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
